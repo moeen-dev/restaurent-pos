806d18913c16a35b96522fa838a2ce2488ffbe47
Moeen Uddin <m.moeen.u9@gmail.com>
2026-03-26 21:50:48 +0600

    Changes sidebar on superadmin dashboard

--- resources/views/backend/partials/sidebar.blade.php
+++ resources/views/backend/partials/sidebar.blade.php
@@ -8,12 +8,8 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="dropdown {{ request()->routeIs('admin.*') ? 'active' : '' }}">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-                <ul class="dropdown-menu">
-                    <li class={{ request()->routeIs('admin.home') ? 'active' : '' }}><a class="nav-link"
-                            href="index-0.html">General Dashboard</a></li>
-                </ul>
+            <li class="dropdown {{ request()->routeIs('admin.home') ? 'active' : '' }}">
+                <a href="{{ route('admin.home') }}" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
             </li>
             <li class="menu-header">Starter</li>
             <li class="dropdown">
